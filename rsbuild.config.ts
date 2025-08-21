b2d2a0e0b42b5c5cf4eb3fb00e40380fcd764ffc
Utsav Barnwal <u.barnwal36@gmail.com>
2025-08-22 00:02:33 +0700

    Add Tailwind CSS integration with PostCSS and update configurations

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  // @ts-ignore -- valid config as of https://tailwindcss.com/docs/installation/framework-guides/rspack/react
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["postcss-loader"],
+        type: "css",
+      },
+    ],
+  },
 });
